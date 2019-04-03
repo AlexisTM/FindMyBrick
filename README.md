ae609f735384fc0cfc06dfc8c5b962714dd686fa
Alexis Paques <alexis.paques@gmail.com>
2019-04-03 10:28:11 +0200

    Serving using HTTPS with auto generation of the SSL key

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# camera-app
-Super awesome camera web app built with HTML, CSS, and JS.
+# Find My Brick
+
+Find My LEGO Brick by using your camera!
+
+Click on the brick you want and others of the same color will pop up!
+
+## Running locally
+
+Open web/index.html with your favorite browser.
+
+## Running on a server
+
+The usage of the camera requires a SSL connection. Therefore, you have to generate an SSL certificate.
+
+You can do it by running the generation script followed by the serve command.
+
+```bash
+openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+python3 serve.py
+```
+
+If you are lazy, you can just run serve.py which will create a key with the `-batch` argument, meaning that the keys are not customized.
+
+```python
+python3 serve.py
+```
--- serve.py
+++ serve.py
@@ -3,12 +3,54 @@
 #    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 #    https://localhost:4443
 
-import http.server, ssl
+import http.server, ssl, os
+from subprocess import check_call
 
-server_address = ('0.0.0.0', 4443)
-httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               server_side=True,
-                               certfile='server.pem',
-                               ssl_version=ssl.PROTOCOL_TLSv1_2)
-httpd.serve_forever()
+IP='0.0.0.0'
+PORT='4443'
+
+config_dir = os.getcwd()
+if __file__:
+    config_dir = os.path.dirname(os.path.realpath(__file__))
+config_pem = os.path.join(config_dir, 'server.pem')
+
+web_dir = os.path.join(config_dir, 'web')
+
+def generate_certificate():
+    os.chdir(config_dir)
+    print(config_dir)
+    check_call(["openssl","req", "-new", "-x509", "-keyout", "server.pem", "-out", "server.pem", "-days", "365", "-nodes", "-batch"])
+
+def serve():
+    try:
+        if not os.path.exists(web_dir):
+            os.makedirs(web_dir)
+        os.chdir(web_dir)
+        print(web_dir)
+        server_address = ('0.0.0.0', 4443)
+        httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+        httpd.socket = ssl.wrap_socket(httpd.socket,
+                                    server_side=True,
+                                    certfile=config_pem,
+                                    ssl_version=ssl.PROTOCOL_TLSv1_2)
+        print('Serving', web_dir, 'at', 'https://' + IP + ':' + PORT)
+        httpd.serve_forever()
+    except Exception as ex:
+        httpd.server_close()
+        raise ex
+
+def main():
+    try:
+        serve()
+    except Exception as ex:
+        try:
+            print(ex)
+            print("Failed to serve, generating the certificate.")
+            generate_certificate()
+            serve()
+        except Exception as ex:
+            print(ex)
+            print("Could not serve even after certificate generation")
+
+if __name__ == "__main__":
+    main()
